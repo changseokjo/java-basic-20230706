f732ca28addc2f69941178114149c5c3768c1c6a
조창석 <yulini1022@gmail.com>
2023-07-11 16:33:32 +0900

    20230711-study

--- src/chapter1/Hello.java
+++ src/chapter1/Hello.java
@@ -3,7 +3,13 @@ package chapter1;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 아래 코드는 콘솔에 출력을 하기위한 코드입니다.
+		
+		/*
+		 * 범위 주석입니다.
+		 */
+		
+		System.out.println(/*"Hello"*/);
 
 	}
 
